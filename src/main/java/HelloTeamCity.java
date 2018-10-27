a42a5ad2a03fa3cbc5fb95b7ffa0c5f646eed04e
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-10-27 22:45:11 +0700

    Init commit

---
+++ src/main/java/HelloTeamCity.java
@@ -0,0 +1,7 @@
+public class HelloTeamCity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Team City!");
+    }
+
+}
---
+++ src/main/java/Library.java
@@ -0,0 +1,6 @@
+public class Library {
+
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+}
