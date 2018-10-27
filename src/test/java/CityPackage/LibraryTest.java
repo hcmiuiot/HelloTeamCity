9f2cda794898da1eb7fdb8ba5d0275b287f79aa9
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-10-28 00:29:00 +0700

    Pls run test ahuhu :(((

--- src/test/java/CityPackage/LibraryTest.java
+++ src/test/java/CityPackage/LibraryTest.java
@@ -2,7 +2,7 @@ package CityPackage;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LibraryTest {
 
