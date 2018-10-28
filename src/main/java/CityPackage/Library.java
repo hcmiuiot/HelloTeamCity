b8c395f3eb7be057f1e4d3d0478e53d2c167bfc1
Khoa Tran <tranduckhoatcu@gmail.com>
2018-10-28 15:21:40 +0700

    Revert "fix a little bug"
    This reverts commit cf34a6802a5b9b0725407ec5e4ebd91cdbb5e5bf.

--- src/main/java/CityPackage/Library.java
+++ src/main/java/CityPackage/Library.java
@@ -3,7 +3,7 @@ package CityPackage;
 public class Library {
 
     public static int sum(int a, int b) {
-        return a+b;
+        return a+b+1;
     }
 
     public static double divide(double a, double b) {
