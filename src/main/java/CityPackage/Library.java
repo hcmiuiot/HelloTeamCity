cf34a6802a5b9b0725407ec5e4ebd91cdbb5e5bf
Khoa Tran <tranduckhoatcu@gmail.com>
2018-10-28 14:51:37 +0700

    fix a little bug

--- src/main/java/CityPackage/Library.java
+++ src/main/java/CityPackage/Library.java
@@ -3,7 +3,7 @@ package CityPackage;
 public class Library {
 
     public static int sum(int a, int b) {
-        return a+b+1;
+        return a+b;
     }
 
     public static double divide(double a, double b) {
