555ac6541aee38af6532c5152f0b86433fe670d6
Khoa Tran <tranduckhoatcu@gmail.com>
2018-10-28 15:41:20 +0700

    Revert "it's too late now to say sorry"
    This reverts commit 86851fcb570284ba3c6be6bdc3acbd2fd518ded4.

--- src/main/java/CityPackage/Library.java
+++ src/main/java/CityPackage/Library.java
@@ -3,14 +3,11 @@ package CityPackage;
 public class Library {
 
     public static int sum(int a, int b) {
-        return a+b;
+        return a+b+1;
     }
 
     public static double divide(double a, double b) {
-        if (b == 0)
-            return 1;
-        else
-            return a/b;
+        return a/b;
     }
 
 }
