83b7c1bc82b47d87a619fb6b855838eb1f6b5a47
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-10-28 13:28:19 +0700

    Pls build on other branch

--- src/main/java/CityPackage/HelloTeamCity.java
+++ src/main/java/CityPackage/HelloTeamCity.java
@@ -3,7 +3,7 @@ package CityPackage;
 public class HelloTeamCity {
 
     public static void main(String[] args) {
-        System.out.println("Hello Team City 555!");
+        System.out.println("Hello Team City 555 IoT!");
     }
 
 }
